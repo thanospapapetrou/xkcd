98108a47bebee0a944723cf990738bc3828d89eb
thanos <thanos.papapetrou@gmail.com>
2017-07-05 00:33:20 +0300

    Added LoggingXkcdSpec

---
+++ src/test/groovy/com/github/thanospapapetrou/xkcd/impl/LoggingXkcdSpec.groovy
@@ -0,0 +1,77 @@
+package com.github.thanospapapetrou.xkcd.impl
+
+import java.lang.reflect.Field
+import java.lang.reflect.Modifier
+import java.util.logging.Logger
+
+import com.github.thanospapapetrou.xkcd.api.Xkcd
+import com.github.thanospapapetrou.xkcd.domain.Comic
+import com.github.thanospapapetrou.xkcd.impl.LoggingXkcd;
+
+import spock.lang.Specification
+
+class LoggingXkcdSpec extends Specification {
+	private static final String LOGGER = 'LOGGER'
+	private static final String MODIFIERS = 'modifiers'
+	
+	private static final int ID = 1024
+
+	private Xkcd xkcd
+	private LoggingXkcd loggingXkcd
+	
+	void setup() {
+		Field logger = LoggingXkcd.getDeclaredField(LOGGER)
+		logger.accessible = true
+		Field modifiers = Field.getDeclaredField(MODIFIERS);
+		modifiers.accessible =true
+		modifiers.setInt(logger, logger.modifiers & (~Modifier.FINAL))
+		logger.set(null, Mock(Logger))
+		xkcd = Mock(Xkcd)
+		loggingXkcd = new LoggingXkcd(xkcd)
+	}
+
+	void 'retrieving an existing comic is delegated to the underlying xkcd and a comic retrieved message is logged as info'() {
+		given: 'a comic'
+			Comic comic = Mock(Comic)
+		when: 'comic is retrieved'
+			Comic result = loggingXkcd.getComic(ID)
+		then: 'retrieval is delegated to the underlying xkcd'
+			1 * xkcd.getComic(ID) >> comic
+		and: 'a comic retrieved message is logged as info'
+			1 * LoggingXkcd.LOGGER.info(String.format(LoggingXkcd.COMIC_RETRIEVED, ID))
+		and: 'no other interactions happen'
+			0 * _
+		and: 'comic is returned'
+			result == comic
+	}
+
+	void 'retrieving a non existing comic is delegated to the underlying xkcd and a comic not found message is logged as info'() {
+		when: 'comic is retrieved'
+			Comic result = loggingXkcd.getComic(ID)
+		then: 'retrieval is delegated to the underlying xkcd'
+			1 * xkcd.getComic(ID) >> null
+		and: 'a comic not found message is logged as info'
+			1 * LoggingXkcd.LOGGER.info(String.format(LoggingXkcd.COMIC_NOT_FOUND, ID))
+		and: 'no other interactions happen'
+			0 * _
+		and: 'null is returned'
+			result == null
+	}
+
+	void 'retrieving current comic is delegated to the underlying xkcd and a current comic retrieved message is logged as info'() {
+		given: 'a comic'
+			Comic comic = Mock(Comic)
+		when: 'current comic is retrieved'
+			Comic result = loggingXkcd.getCurrentComic()
+		then: 'retrieval is delegated to the underlying xkcd'
+			1 * xkcd.getCurrentComic() >> comic
+		and: 'the comic ID is retrieved'
+			1 * comic.getId() >> ID
+		and: 'a current comic retrieved message is logged as info'
+			1 * LoggingXkcd.LOGGER.info(String.format(LoggingXkcd.CURRENT_COMIC_RETRIEVED, ID))
+		and: 'no other interactions happen'
+			0 * _
+		and: 'comic is returned'
+			result == comic
+	}
+}
--- src/test/groovy/com/github/thanospapapetrou/xkcd/impl/RootRedirectorSpec.groovy
+++ src/test/groovy/com/github/thanospapapetrou/xkcd/impl/RootRedirectorSpec.groovy
@@ -6,12 +6,13 @@ import spock.lang.Specification
 
 class RootRedirectorSpec extends Specification {
 	def 'Accessing root path redirects to current comic'() {
-	given: 'an HTTP servlet response'
-		HttpServletResponse response = Mock(HttpServletResponse)
-	when: 'an HTTP GET request is performed on root path of the web application'
-		new RootRedirector().doGet(null, response)
-	then: 'a redirection to current comic is sent as response'
-		1 * response.sendRedirect(RootRedirector.CURRENT_COMIC)
+		given: 'an HTTP servlet response'
+			HttpServletResponse response = Mock(HttpServletResponse)
+		when: 'an HTTP GET request is performed on root path of the web application'
+			new RootRedirector().doGet(null, response)
+		then: 'a redirection to current comic is sent as response'
+			1 * response.sendRedirect(RootRedirector.CURRENT_COMIC)
+		and: 'no other interactions happen'
+			0 * _
 	}
 }
-
